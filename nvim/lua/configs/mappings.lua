64f7037606f2f68ef60e2a390c449b347332b13c
Unai Gonzalez Sanchez <unaigsanchez@gmail.com>
2026-03-12 18:22:20 +0100

    adding some usefull shortcuts

--- nvim/lua/configs/mappings.lua
+++ nvim/lua/configs/mappings.lua
@@ -2,10 +2,8 @@
 vim.keymap.set("x", "<leader>re", ":Refactor extract ")
 vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file ")
 vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ")
-
 vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
 vim.keymap.set("n", "<leader>rI", ":Refactor inline_func")
-
 vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
 vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
 
@@ -19,20 +17,21 @@ end, { desc = "Print variable" })
 vim.keymap.set("n", "<leader>dc", function()
 	require("refactoring").debug.cleanup({})
 end, { desc = "Clear prints" })
-vim.keymap.set("n", "<leader>db", function()
+
+vim.keymap.set("n", "<F8>", function()
 	require("dap").toggle_breakpoint()
 end, { desc = "Toggle breakpoint" })
-vim.keymap.set("n", "<leader>dt", function()
+vim.keymap.set("n", "<F10>", function()
 	require("dap").terminate()
 end, { desc = "Terminate debugger" })
-vim.keymap.set("n", "<leader>dr", function()
+vim.keymap.set("n", "<F9>", function()
 	require("dap").continue()
 end, { desc = "Start debugger" })
 vim.keymap.set("n", "<leader>dac", function()
 	require("dap").clear_breakpoints()
 end, { desc = "Clear breakpoints" })
 
--- COMMENTOR MAPPING
+--COMMENTOR MAPPING
 vim.keymap.set("", "]t", function()
 	require("todo-comments").jump_next()
 end, { desc = "Next Todo Comment" })
@@ -42,7 +41,7 @@ end, { desc = "Previous Todo Comment" })
 vim.keymap.set("", "<leader>tt", "<cmd>TodoTelescope<cr>", { desc = "Todo" })
 vim.keymap.set("", "<leader>tT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", { desc = "Todo/Fix/Fixme" })
 
--- OUTLINER MAPPING
+--OUTLINER MAPPING
 vim.keymap.set("", "<leader>o", "<cmd>Outline!<CR>", { desc = "Toggle outline" })
 
 --SESSION MANAGMENT
@@ -67,3 +66,42 @@ vim.keymap.set({ "i" }, "<C-g>", function()
 end, { silent = true, noremap = true, desc = "Toggle signature" })
 
 vim.keymap.del("i", "<C-s>")
+
+--COMPILER & BUILDER
+local term = nil
+local job = nil
+vim.keymap.set({ "i", "v", "n" }, "<F5>", function()
+	if term == nil then
+		vim.cmd("new | terminal")
+		vim.bo.buflisted = false
+		term = vim.api.nvim_get_current_buf()
+		job = vim.b.terminal_job_id
+		vim.api.nvim_create_autocmd("BufWinLeave", {
+			buffer = term,
+			callback = function()
+				term = nil
+				job = nil
+			end,
+		})
+	end
+	vim.fn.chansend(job, "cmb\n")
+end)
+
+vim.keymap.set({ "i", "v", "n" }, "<F6>", function()
+	if term == nil or not vim.api.nvim_buf_is_valid(term) then
+		vim.cmd("new | terminal")
+		vim.bo.buflisted = false
+		term = vim.api.nvim_get_current_buf()
+		job = vim.b.terminal_job_id
+		vim.api.nvim_create_autocmd("BufWinLeave", {
+			buffer = term,
+			callback = function()
+				term = nil
+				job = nil
+			end,
+		})
+	end
+	local dir = vim.fn.getcwd()
+	local dir_name = vim.fn.fnamemodify(dir, ":t")
+	vim.fn.chansend(job, "build/" .. dir_name .. "\n")
+end)
