52aaf5c01759ee4a240470023f8210b34826803e
Unai Gonzalez Sanchez <unaigsanchez@gmail.com>
2026-03-28 13:59:21 +0100

    unai | dotfiles ➤ git restore

--- templates/project_builder.sh
+++ templates/project_builder.sh
@@ -32,7 +32,8 @@ project_builder() {
 
   mkdir -p "$path/"{src,build}
   touch "$path/src/main.cpp"
-  echo -e "project($name)\n\nset(CMAKE_CXX_STANDARD_REQUIRED ON)\nset(CMAKE_EXPORT_COMPILE_COMMANDS ON)\nset(CMAKE_CXX_SCAN_FOR_MODULES OFF)\n\nadd_executable($name src/main.cpp)" > "$path/CMakeLists.txt"
+  echo -e "project($name)\n\nset(CMAKE_CXX_STANDARD_REQUIRED ON)\nset(CMAKE_EXPORT_COMPILE_COMMANDS ON)\nset(CMAKE_CXX_SCAN_FOR_MODULES OFF)
+\n\nadd_executable($name src/main.cpp)" > "$path/CMakeLists.txt"
 
   local format_file="$HOME/Templates/cpp/.clang-format"
   if [ -f "$format_file" ]; then
